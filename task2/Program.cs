530e485321287a31cac96b148ea2402934e84623
Anna Dorosheva <dorosheva.av@gmail.com>
2023-09-24 13:18:22 +0300

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,3 +2,31 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+
+
+int a, b;
+
+Console.WriteLine("Enter number A: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number B: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+if (a == b) 
+{
+    Console.WriteLine("A = B");
+}
+else 
+{
+    if (a > b) 
+{
+    Console.WriteLine("A > B");
+}
+else 
+{
+    Console.WriteLine("B > A");
+}
+}
