20845429eb62fa6c06d70382488fc1012c6bf5a9
Anna Dorosheva <dorosheva.av@gmail.com>
2023-09-24 13:32:08 +0300

    task6 done

--- task6/Program.cs
+++ task6/Program.cs
@@ -2,3 +2,20 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
+
+
+int a;
+
+Console.WriteLine("Enter number A: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (a % 2 == 0) 
+{
+    Console.WriteLine("A is an even number");
+}
+else 
+{
+    Console.WriteLine("A is an odd number");
+}
